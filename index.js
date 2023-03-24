b40217255b14fb48f58c16380a6660bf3eaad21c
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-03-24 19:28:17 -0400

    UserS-5 completed

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // Generate random number
 const generateRandomNumber = () => {
   const randomNumber = Math.floor(Math.random() * 1000) + 1;
-  console.log(randomNumber);
   return randomNumber;
 };
 
@@ -13,7 +12,6 @@ const fetchData = async () => {
     `https://pokeapi.co/api/v2/pokemon/${generateRandomNumber()}/`
   );
   const data = await req.json();
-  //   console.log(data);
   return data;
 };
 const setCards = async (
@@ -37,7 +35,7 @@ const setCards = async (
   type1.textContent = cardData.types[0].type.name;
   //    Set the type 2 text only if it exists
   const type2 = document.querySelector(type2Class);
-  type2.textContent = cardData.types[1] ? cardData.types[1].type.name : "none";
+  type2.textContent = cardData.types[1] ? cardData.types[1].type.name : "N/A";
   //   Set the image
   //   The deffault image is the bulbasaur one
   const img = document.querySelector(imgClass);
@@ -59,7 +57,8 @@ const setCards = async (
   speed.textContent = `SPEED: ${cardData.stats[5].base_stat}`;
 };
 
-for (let i = 0; i <= 8; i++) {
+// Create 12 cards
+for (let i = 0; i <= 12; i++) {
   setCards(
     `.name-${i}`,
     `.type-${i}-1`,
@@ -71,98 +70,3 @@ for (let i = 0; i <= 8; i++) {
     `.speed-${i}`
   );
 }
-// Card 1
-// setCards(
-//   ".name-1",
-//   ".type-1-1",
-//   ".type-1-2",
-//   ".img-1",
-//   ".hp-1",
-//   ".attack-1",
-//   ".defense-1",
-//   ".speed-1"
-// );
-
-// // Card 2
-// setCards(
-//   ".name-2",
-//   ".type-2-1",
-//   ".type-2-2",
-//   ".img-2",
-//   ".hp-2",
-//   ".attack-2",
-//   ".defense-2",
-//   ".speed-2"
-// );
-
-// // Card 3
-// setCards(
-//   ".name-3",
-//   ".type-3-1",
-//   ".type-3-2",
-//   ".img-3",
-//   ".hp-3",
-//   ".attack-3",
-//   ".defense-3",
-//   ".speed-3"
-// );
-
-// // Card 4
-// setCards(
-//   ".name-4",
-//   ".type-4-1",
-//   ".type-4-2",
-//   ".img-4",
-//   ".hp-4",
-//   ".attack-4",
-//   ".defense-4",
-//   ".speed-4"
-// );
-
-// // Card 5
-// setCards(
-//   ".name-5",
-//   ".type-5-1",
-//   ".type-5-2",
-//   ".img-5",
-//   ".hp-5",
-//   ".attack-5",
-//   ".defense-5",
-//   ".speed-5"
-// );
-
-// // Card 6
-// setCards(
-//   ".name-6",
-//   ".type-6-1",
-//   ".type-6-2",
-//   ".img-6",
-//   ".hp-6",
-//   ".attack-6",
-//   ".defense-6",
-//   ".speed-6"
-// );
-
-// // Card 6
-// setCards(
-//   ".name-7",
-//   ".type-7-1",
-//   ".type-7-2",
-//   ".img-7",
-//   ".hp-7",
-//   ".attack-7",
-//   ".defense-7",
-//   ".speed-7"
-// );
-
-// // Card 8
-// setCards(
-//   ".name-8",
-//   ".type-8-1",
-//   ".type-8-2",
-//   ".img-8",
-//   ".hp-8",
-//   ".attack-8",
-//   ".defense-8",
-//   ".speed-8"
-// );
